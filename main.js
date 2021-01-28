d3bd8d6b99139e53969bd4be9fd0b5f33019c107
Ethan600 <77286763+Ethan600@users.noreply.github.com>
2021-01-28 14:16:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function spanalert(){
+    window.alert("This is Percy Jackson and the Lightning Theif By Rick Riordan");
+}
+function spanalert2(){
+    window.alert("This is Percy Jackson and the Sea Of Monsters By Rick Riordan");
+}
+function spanalert3(){
+    window.alert("This is Percy Jackson and the Titans Curse By Rick Riordan");
+}
+function spanalert4(){
+    window.alert("This is Percy Jackson and the Battle Of The Labyrinth By Rick Riordan");
+}
+function spanalert5(){
+    window.alert("This is the Fowl Twins (after the Artemis Fowl series) By Eoin Colfer");
+}
+function spanalert6(){
+    window.alert("This is Diary of a Wimpy Kid The Deep End By Jeff Kinney");
+}
+function spanalert7(){
+    window.alert("This is Diary of a Wimpy Kid Double Down By Jeff Kinney");
+}
+function spanalert8(){
+    window.alert("This is The Thousand Year Old Boy By Ross Welford");
+}
